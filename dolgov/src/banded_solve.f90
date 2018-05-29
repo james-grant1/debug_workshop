985ba19961893b482fa459a4d230e8268a4df42e
Sergey Dolgov <sergey.v.dolgov@gmail.com>
2018-05-29 01:21:36 +0100

    bug overview

--- dolgov/src/banded_solve.f90
+++ dolgov/src/banded_solve.f90
@@ -17,18 +17,18 @@ subroutine banded_solve(n,M1,M2,S,W,U)
   integer :: info
   real(kind=8), dimension(4,n) :: Mtmp
 
-  call dcopy(2*n, W, 1, U, 1)
-  
   ! Backup the matrix
   call dcopy(4*n, M1, 1, Mtmp, 1)
   ! solve u1 = M1^{-1}*W1
-  call dgbsv(n, 1,1, 1, Mtmp, 4, ipiv, U(1), n, info)
+  call dgbsv(n, 1,1, 1, Mtmp, 4, ipiv, W(1), n, info)
   
   ! form g2 = W2 + S*u1
-  call dsbmv('U', n, 0, 1d0, S, 1, U(1), 1, 1d0, U(n+1), 1)
+  call dsbmv('U', n, 0, 1d0, S, 1, W(1), 1, 1d0, W(n+1), 1)
   
   ! solve u2 = M2^{-1}*g2
   call dcopy(4*n, M2, 1, Mtmp, 1)
-  call dgbsv(n, 1,1, 1, Mtmp, 4, ipiv, U(n+1), n, info)
+  call dgbsv(n, 1,1, 1, Mtmp, 4, ipiv, W(n+1), n, info)
+
+  call dcopy(2*n, W, 1, U, 1)
     
 end subroutine banded_solve
